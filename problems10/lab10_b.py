e31cb2baba862bf9976dc756b62067213864b46d
George Lopatenko <lopa10ko239@mail.ru>
2022-03-23 00:00:51 +0300

    add: lab10_b lame

--- problems10/lab10_b.py
+++ problems10/lab10_b.py
@@ -1,38 +1,26 @@
-from sys import setrecursionlimit
-import threading
-setrecursionlimit(10 ** 9)
-threading.stack_size(67108864)
- 
-def main():
-    file_input, file_output = open('topsort.in', 'r'), open('topsort.out','w')
-    #dfs for each vertex in V; vertex_f --> time
-    #for each all-done vertex push to linked_lst
-    def dfs(index):
-        contig_lst[index][0] = 1
-        for i in range(1, len(contig_lst[index])):
-            if contig_lst[contig_lst[index][i]][0] == 0:
-                if dfs(contig_lst[index][i]): return True
-            elif contig_lst[contig_lst[index][i]][0] == 1: return True
-        contig_lst[index][0] = 2
-        linked_lst.append(index + 1)
+file_input, file_output = open("spantree.in", "r"), open("spantree.out", "w")
 
-    n, m= map(int, file_input.readline().split())
-    contig_lst, linked_lst= [[0] for _ in range(n)], []
-    flag = 0
-    #contigious vertexes list creation i: [color, *[ends_values]]
-    for _ in range(m):
-        current = list(map(int, file_input.readline().split()))
-        contig_lst[current[0] - 1].append(current[1] - 1)
-    for index in range(len(contig_lst)):
-        # print(*contig_lst)
-        if contig_lst[index][0] == 0:
-            if dfs(index):
-                print(-1, file=file_output)
-                flag = 1
-                break
-    if not flag: 
-        print(*linked_lst[::-1], sep=' ', file=file_output)
-    file_output.close()
+n = int(file_input.readline())
+max_verge = float("inf")
+#all_weights: [(marked/not marked), [list of weigts between current vertex and others]]
+all_weights = [[None, []] for _ in range(n)]
+vertex_xy = [list(map(int, file_input.readline().split())) for _ in range(n)]
+weight_lst = [0] + [max_verge for _ in range(1, n)]
+for i in range(n):
+    point1 = vertex_xy[i]
+    for j in range(n):
+        point2 = vertex_xy[j]
+        all_weights[i][1].append((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+for _ in range(n):
+    current = None
+    for j in range(n):
+        #if not marked and (is_first_vertex or is_min(list of all weights to one vertex))
+        if not all_weights[j][0] and (current is None or weight_lst[j] < weight_lst[current]):
+            current = j
+    all_weights[current][0] = 1
+    for vertex in range(n):
+        if not all_weights[vertex][0] and all_weights[current][1][vertex] < weight_lst[vertex] and vertex is not current:
+            weight_lst[vertex] = all_weights[current][1][vertex]
+print(sum(verge ** 0.5 for verge in weight_lst), file = file_output)
 
-thread = threading.Thread(target=main)
-thread.start()
+file_output.close()
